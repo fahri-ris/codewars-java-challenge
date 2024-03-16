f0b7aa29afef8597eaecaa0dea3ff0e0407e75d5
fahri-ris <fahri.ris15@gmail.com>
2024-03-16 14:23:40 +0700

    EvenOrOdd kyu 8

---
+++ src/main/java/com/codewarsjava/kyu8/EvenOrOdd.java
@@ -0,0 +1,26 @@
+package com.codewarsjava.kyu8;
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        String result = cekEvenOdd(a);
+
+        System.out.println(result);
+    }
+
+    public static String cekEvenOdd(int number){
+//        String result;
+//        if(number % 2 == 0){
+//            result = "Even";
+//        } else {
+//            result = "Odd";
+//        }
+//
+//        return result;
+
+        // Cara cepat
+        return number % 2 == 0? "Even" : "Odd";
+    }
+}
